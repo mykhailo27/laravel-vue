eb499d50a73988fcdb864ba1e28f347b82991f35
mykhailo27 <mykhailo.yur27@gmail.com>
2023-05-26 00:52:18 +0200

    remove logo from the breadcrumb place

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -28,21 +28,10 @@ const showingNavigationDropdown = ref(false);
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="flex justify-between h-16">
                         <div class="flex">
-                            <!-- Logo -->
-                            <div class="shrink-0 flex items-center">
-                                <Link :href="route('dashboard')">
-                                    <ApplicationLogo
-                                        class="block h-9 w-auto fill-current text-gray-800"
-                                    />
-                                </Link>
-                            </div>
-
                             <!-- Navigation Links -->
-                            <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                                <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
-                                    Dashboard
-                                </NavLink>
-                            </div>
+                            <NavLink :href="route('dashboard')" :active="route().current('dashboard')">
+                                Dashboard
+                            </NavLink>
                         </div>
 
                         <div class="hidden sm:flex sm:items-center sm:ml-6">
