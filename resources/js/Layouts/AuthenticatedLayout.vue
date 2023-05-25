be98275e1c4f56940cec5d768c6e070e59ff4031
mykhailo27 <mykhailo.yur27@gmail.com>
2023-05-26 00:32:49 +0200

    update loyout

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -6,13 +6,23 @@ import DropdownLink from '@/Components/DropdownLink.vue';
 import NavLink from '@/Components/NavLink.vue';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink.vue';
 import { Link } from '@inertiajs/vue3';
+import SidenavToggler from "@/Layouts/SidenavToggler.vue";
+import Sidenav from "@/Layouts/Sidenav.vue";
 
 const showingNavigationDropdown = ref(false);
 </script>
 
 <template>
-    <div>
-        <div class="min-h-screen bg-gray-100">
+    <div class="max-h-screen overflow-auto">
+
+        <!-- Page Sidenav -->
+        <sidenav />
+
+        <div class="page-content sticky top-0">
+            <!-- Sidenav Toggler -->
+            <sidenav-toggler/>
+
+            <!-- Page Navigation -->
             <nav class="bg-white border-b border-gray-100">
                 <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -142,11 +152,19 @@ const showingNavigationDropdown = ref(false);
                     <slot name="header" />
                 </div>
             </header>
-
-            <!-- Page Content -->
-            <main>
-                <slot />
-            </main>
         </div>
+
+        <!-- Page Content -->
+        <main class="page-content">
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-lg sm:rounded-lg">
+                        <div class="p-6 text-gray-900 dark:text-gray-100">
+                            <slot />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
     </div>
 </template>
