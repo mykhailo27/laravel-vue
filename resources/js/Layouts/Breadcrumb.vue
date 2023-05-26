830d785f9acb0c4b9d34b82ed1fad9357cd63621
mykhailo27 <mykhailo.yur27@gmail.com>
2023-05-26 23:17:20 +0200

    remove the route use the path directly

--- resources/js/Layouts/Breadcrumb.vue
+++ resources/js/Layouts/Breadcrumb.vue
@@ -30,7 +30,7 @@ onMounted(function () {
     <nav v-if="links.length > 0" class="bg-grey-light w-full rounded-md">
         <ol class="list-reset flex text-sm items-center capitalize">
             <li v-for="(link, index) in links">
-                <Link :content-classes="null" v-if="links.length > index + 1" :href="route(link.route)">
+                <Link :content-classes="null" v-if="links.length > index + 1" :href="link.route">
                     {{ link.name }}
                     <span class="px-2 text-neutral-500 dark:text-neutral-400">/</span>
                 </Link>
