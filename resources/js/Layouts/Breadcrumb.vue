21aada3f5a09a682fc5f0cf3f13f2d21f9970158
mykhailo27 <mykhailo.yur27@gmail.com>
2023-05-26 23:27:32 +0200

    revert the change

--- resources/js/Layouts/Breadcrumb.vue
+++ resources/js/Layouts/Breadcrumb.vue
@@ -2,6 +2,7 @@
 
 import Link from "@/Components/Link.vue";
 import {onMounted, ref} from "vue";
+import {router} from "@inertiajs/vue3";
 
 const links = ref([])
 
@@ -11,7 +12,7 @@ function updateVisitedLinks() {
     return location.pathname.split('/')
         .filter(crumb => crumb !== '')
         .map(crumb => {
-            current_link += `/${crumb}`;
+            current_link += `${crumb}`;
 
             return {
                 name: crumb,
@@ -30,7 +31,7 @@ onMounted(function () {
     <nav v-if="links.length > 0" class="bg-grey-light w-full rounded-md">
         <ol class="list-reset flex text-sm items-center capitalize">
             <li v-for="(link, index) in links">
-                <Link :content-classes="null" v-if="links.length > index + 1" :href="link.route">
+                <Link :content-classes="null" v-if="links.length > index + 1" :href="route(link.route)">
                     {{ link.name }}
                     <span class="px-2 text-neutral-500 dark:text-neutral-400">/</span>
                 </Link>
