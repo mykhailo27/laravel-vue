5eedaf66ec030d210fb9306e31bab41f5b31d841
mykhailo27 <mykhailo.yur27@gmail.com>
2023-05-26 20:29:01 +0200

    add class

--- resources/js/Components/Link.vue
+++ resources/js/Components/Link.vue
@@ -5,13 +5,17 @@ const props = defineProps({
     href: {
         type: String,
         required: true,
-    }
+    },
+    contentClasses: {
+        type: String,
+        default: 'px-6 py-3.5',
+    },
 });
 
 </script>
 
 <template>
-    <Link :href="href" class="flex items-center justify-start px-6 py-3.5 outline-none">
+    <Link :href="href" class="flex items-center justify-start outline-none" :class="contentClasses">
         <slot />
     </Link>
 </template>
