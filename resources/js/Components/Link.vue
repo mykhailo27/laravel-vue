389272fd1c62e29f7d5fef4de1e0bca9ce8a2a9b
mykhailo27 <mykhailo.yur27@gmail.com>
2023-05-26 19:40:17 +0200

    create Link a tag

---
+++ resources/js/Components/Link.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+const props = defineProps({
+    href: {
+        type: String,
+        required: true,
+    }
+});
+
+</script>
+
+<template>
+    <Link :href="href" class="flex items-center justify-start px-6 py-3.5 outline-none">
+        <slot />
+    </Link>
+</template>
