91c87dfeb46db375bbc2c324e7e4698a54c82a1f
mykhailo27 <mykhailo.yur27@gmail.com>
2023-05-27 18:18:19 +0200

    add link to the agencies route

--- resources/js/Layouts/Sidenav.vue
+++ resources/js/Layouts/Sidenav.vue
@@ -287,7 +287,7 @@ onMounted(function () {
                 </a>
             </li>
             <li class="relative">
-                <a href="{{ route('agencies.index') }}"
+                <Link :href="route('agencies.index')"
                    class="flex cursor-pointer items-center truncate rounded-[5px] px-6 py-[0.45rem] text-[0.85rem] text-gray-600 outline-none transition duration-300 ease-linear hover:bg-slate-50 hover:text-inherit hover:outline-none focus:bg-slate-50 focus:text-inherit focus:outline-none active:bg-slate-50 active:text-inherit active:outline-none data-[te-sidenav-state-active]:text-inherit data-[te-sidenav-state-focus]:outline-none motion-reduce:transition-none dark:text-gray-300 dark:hover:bg-white/10 dark:focus:bg-white/10 dark:active:bg-white/10"
                    data-te-sidenav-link-ref>
         <span
@@ -304,7 +304,7 @@ onMounted(function () {
           </svg>
         </span>
                     <span>Agencies</span>
-                </a>
+                </Link>
             </li>
             <li class="relative">
                 <a
