d349168c687d3316f790bdc86623ed26cde4b148
mykhailo27 <mykhailo.yur27@gmail.com>
2023-05-28 03:00:25 +0200

    change the padding

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -141,7 +141,7 @@ const showingNavigationDropdown = ref(false);
 
         <!-- Page Content -->
         <main class="page-content">
-            <div class="py-12">
+            <div class="py-6">
                 <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-lg sm:rounded-lg">
                         <div class="p-6 text-gray-900 dark:text-gray-100">
