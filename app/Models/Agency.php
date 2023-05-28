7d5d4426e8613ea1b289168ffaaad8e435a59d53
mykhailo27 <mykhailo.yur27@gmail.com>
2023-05-28 02:41:28 +0200

    add paginate @method and SoftDeletes traits

--- app/Models/Agency.php
+++ app/Models/Agency.php
@@ -5,16 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
  * @property string $id
  * @property string $name
  * @property string $email
  * @method static create(string[] $array)
+ * @method static paginate(int $int)
  */
 class Agency extends Model
 {
-    use HasFactory, HasUuids;
+    use HasFactory, HasUuids, SoftDeletes;
 
     protected $fillable = ['name', 'email'];
 }
