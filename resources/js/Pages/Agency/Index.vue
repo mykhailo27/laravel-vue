f9231a5bae622a7f7157a87a310bc2fa85157c8e
mykhailo27 <mykhailo.yur27@gmail.com>
2023-05-28 20:23:16 +0200

    handle table and row checkbox and update the selected_id accordingly

--- resources/js/Pages/Agency/Index.vue
+++ resources/js/Pages/Agency/Index.vue
@@ -15,45 +15,65 @@ import {ref} from "vue";
 defineProps({
     agencies: {
         type: Object,
-        required: true
+        required: true,
     },
 });
 
 const selected_id = ref([]);
-
 const table_checkbox = 'table-checkbox'
 const form = useForm({});
 
-const deleteAgency = (id) => {
+const deleteAgency = (id => {
     form.delete(route('agencies.destroy', {agency: id}), {
         preserveScroll: true,
         // onSuccess: () => closeModal(),
         // onError: () => passwordInput.value.focus(),
         // onFinish: () => form.reset(),
     });
-};
+});
 
-function details(id) {
+const details = (id => {
     window.location.href = route('agencies.details', {agency: id})
-}
+})
 
-function selectAll(event) {
+const updateSelectedId = (row_checkbox => {
+    const value = row_checkbox.value;
+    const index = selected_id.value.indexOf(value)
+
+    if (row_checkbox.checked && index === -1) {
+        selected_id.value.push(value)
+    } else if (index !== -1) {
+        selected_id.value.splice(index, 1);
+    }
+})
+
+const handleTableCheckboxClick = (event => {
     const target = event.target;
     const checked = target.checked;
-
-    if (!checked) {
-        selected_id.value = [];
-    }
 
     target.closest('table')
         .querySelectorAll('tbody tr input')
         .forEach(input => {
             input.checked = checked
-            if (checked) {
-                selected_id.value.push(input.value)
-            }
+            updateSelectedId(input)
         })
-}
+})
+
+const handleRowCheckboxClick = (event => {
+    const target = event.target;
+
+    updateSelectedId(target);
+
+    const all_row_checkbox = target.closest('table')
+        .querySelectorAll('tbody tr input[type="checkbox"]');
+
+    const checked_rows_checkbox = [...all_row_checkbox]
+        .filter(row_checkbox => row_checkbox.checked);
+
+    target.closest('table')
+        .querySelector('thead th input')
+        .checked = all_row_checkbox.length === checked_rows_checkbox.length
+})
 
 </script>
 
@@ -68,13 +88,14 @@ function selectAll(event) {
         </template>
 
         <div class="">
-            <Pagination :links="agencies.links" />
+            <Pagination :links="agencies.links"/>
         </div>
 
         <Table>
             <template #columns>
                 <Th>
-                    <TextInput type="checkbox" @click="selectAll" id="table-checkbox" :model-value="table_checkbox"/>
+                    <TextInput type="checkbox" @click="handleTableCheckboxClick" id="table-checkbox"
+                               :model-value="table_checkbox"/>
                 </Th>
                 <Th>Name</Th>
                 <Th>Email</Th>
@@ -84,7 +105,8 @@ function selectAll(event) {
             <template #rows>
                 <Tr v-for="agency in agencies.data" @click="details(agency.id)">
                     <Td>
-                        <TextInput type="checkbox" :model-value="agency.id" @click.stop/>
+                        <TextInput type="checkbox" :model-value="agency.id" @click.stop
+                                   @click="handleRowCheckboxClick"/>
                     </Td>
                     <Td>{{ agency.name }}</Td>
                     <Td>{{ agency.email }}</Td>
