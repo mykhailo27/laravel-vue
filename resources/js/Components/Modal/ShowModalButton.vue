1df8e399f9199690e0e7a0ecfeb2cc17be8d194c
mykhailo27 <mykhailo.yur27@gmail.com>
2023-05-29 22:07:28 +0200

    create modal and the show button of it

---
+++ resources/js/Components/Modal/ShowModalButton.vue
@@ -0,0 +1,23 @@
+<script setup>
+import {initTE, Ripple} from "tw-elements";
+import {onMounted} from "vue";
+import PrimaryButton from "@/Components/PrimaryButton.vue";
+
+onMounted(function () {
+    initTE({Ripple});
+})
+</script>
+
+<template>
+    <PrimaryButton
+        type="button"
+        data-te-toggle="modal"
+        data-te-ripple-init
+        data-te-ripple-color="light">
+        <slot/>
+    </PrimaryButton>
+</template>
+
+<style scoped>
+
+</style>
