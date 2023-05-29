b6a82fab31516ae12227ab22f29acc3263fe16f5
mykhailo27 <mykhailo.yur27@gmail.com>
2023-05-29 21:57:50 +0200

    create relation with user and add hasUser, nonAgencyUsers and addUser mathods

--- app/Models/Agency.php
+++ app/Models/Agency.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 /**
@@ -19,4 +20,34 @@ class Agency extends Model
     use HasFactory, HasUuids, SoftDeletes;
 
     protected $fillable = ['name', 'email'];
+
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class)
+            ->using(AgencyUser::class)
+            ->withPivot('selected')
+            ->withTimestamps();
+    }
+
+    public function hasUser(User $user): bool
+    {
+        return $this->users()
+            ->wherePivot('user_id', '=', $user->id)
+            ->exists();
+    }
+
+    public function addUser(User $user): bool
+    {
+        $this->users()->attach($user->id);
+
+        return $this->hasUser($user);
+    }
+
+    public function nonAgencyUser()
+    {
+        $users_id = $this->users()->pluck('user_id')->toArray();
+
+        return User::whereNotIn('id', $users_id)->get();
+    }
 }
