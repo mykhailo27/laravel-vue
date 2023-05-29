7a27a08948a931e8cba3c2fc81650cb7cdde44cc
mykhailo27 <mykhailo.yur27@gmail.com>
2023-05-29 22:06:33 +0200

    create pivot table for agency user relation ship

---
+++ database/migrations/2023_05_29_141821_create_agency_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('agency_user', static function (Blueprint $table) {
+            $table->uuid('id');
+            $table->boolean('selected')->default(false);
+            $table->string('agency_id');
+            $table->bigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('agency_user');
+    }
+};
