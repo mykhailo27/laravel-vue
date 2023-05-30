60418488ec855d1328ee1b2adc327f80a788e811
mykhailo27 <mykhailo.yur27@gmail.com>
2023-05-30 23:59:48 +0200

    update the database seeder and user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,9 +15,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
         $this->call([
             AgencySeeder::class,
-            UserSeeder::class
         ]);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,11 +15,6 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-             'name' => 'Test User',
-             'email' => 'test@example.com',
-         ]);
+         User::factory(10)->create();
     }
 }
