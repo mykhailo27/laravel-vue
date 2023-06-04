f9fe771d9bb84804f7d78f74c4edd1d0be4348c9
mykhailo27 <mykhailo.yur27@gmail.com>
2023-06-04 20:42:13 +0200

    user form request for validation

--- app/Http/Controllers/User/UserViewController.php
+++ app/Http/Controllers/User/UserViewController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\User\StoreUserRequest;
+use App\Http\Requests\User\UpdateUserRequest;
 use App\Models\User;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
 use Inertia\Inertia;
 use Inertia\Response;
 
@@ -27,5 +31,39 @@ class UserViewController extends Controller
         return Inertia::render('User/Details', [
             'user' => $user
         ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreUserRequest $request): RedirectResponse
+    {
+        $user = User::create($request->validated());
+
+        return Redirect::route('users.details', [
+            'user' => $user->id
+        ])->with('message', 'user-created');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(): Response
+    {
+        return Inertia::render('User/Details', [
+            'user' => null,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateUserRequest $request, User $user): RedirectResponse
+    {
+        $user->update($request->validated());
+
+        return Redirect::route('users.details', [
+            'user' => $user->id
+        ])->with('message', 'agency-updated');
     }
 }
