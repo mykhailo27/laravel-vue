7491e0306983bef1670d47dcfa238bef6ef797f7
mykhailo27 <mykhailo.yur27@gmail.com>
2023-06-04 22:36:12 +0200

    create user index page

---
+++ resources/js/Pages/User/Index.vue
@@ -0,0 +1,36 @@
+<script setup>
+
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import {Head} from '@inertiajs/vue3';
+import PageTable from "@/Components/Pages/Table.vue";
+import Link from "@/Components/Link.vue";
+
+defineProps({
+    users: {
+        type: Object,
+        required: true,
+    },
+});
+
+</script>
+
+<template>
+    <Head title="Users"/>
+
+    <AuthenticatedLayout>
+        <template #header>
+            <Link :href="route('users.create')"
+                  class="font-semibold border rounded leading-none w-fit hover:bg-gray-200 hover:shadow-lg active:bg-gray-200 active:shadow-lg">
+                Create
+            </Link>
+        </template>
+
+        <PageTable :columns="['id', 'name', 'email', 'created_at', 'actions']" :data="users"
+                   :details_route="route('users.details', {user: '__ROW_ID__'})"/>
+
+    </AuthenticatedLayout>
+</template>
+
+<style scoped>
+
+</style>
