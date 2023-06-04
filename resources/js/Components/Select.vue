5c73cdb84aae59e3fa18a550989cb1456d61456c
mykhailo27 <mykhailo.yur27@gmail.com>
2023-06-04 22:08:46 +0200

    auto select the default option

--- resources/js/Components/Select.vue
+++ resources/js/Components/Select.vue
@@ -1,6 +1,6 @@
 <script setup>
 
-import { Select, initTE } from "tw-elements";
+import {initTE, Select} from "tw-elements";
 import {onMounted} from "vue";
 
 defineProps({
@@ -11,18 +11,24 @@ defineProps({
     label: {
         type: String,
         default: 'select'
+    },
+    default_option: {
+        type: Object
     }
 })
 
 onMounted(function () {
-    initTE({ Select });
+    initTE({Select});
 })
 
 </script>
 
 <template>
-    <select data-te-select-init data-te-select-auto-select="true" class="capitalize">
-        <option v-for="option in options" value="{{ option.value }}">{{ option.name }}</option>
+    <select data-te-select-init data-te-select-auto-select="true" class="capitalize"
+            v-model="default_option.value">
+        <option v-for="option in options" value="{{ option.value }}">
+            {{ option.name }}
+        </option>
     </select>
     <label data-te-select-label-ref class="capitalize">{{ label }}</label>
 </template>
