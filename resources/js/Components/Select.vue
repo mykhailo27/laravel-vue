c443a5b8a58d7cd2b8e3b1ef529502154cef4456
mykhailo27 <mykhailo.yur27@gmail.com>
2023-06-04 21:21:22 +0200

    create a select component

---
+++ resources/js/Components/Select.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+import { Select, initTE } from "tw-elements";
+import {onMounted} from "vue";
+
+defineProps({
+    options: {
+        type: Array,
+        required: true
+    },
+    label: {
+        type: String,
+        default: 'select'
+    }
+})
+
+onMounted(function () {
+    initTE({ Select });
+})
+
+</script>
+
+<template>
+    <select data-te-select-init data-te-select-auto-select="true" class="capitalize">
+        <option v-for="option in options" value="{{ option.value }}">{{ option.name }}</option>
+    </select>
+    <label data-te-select-label-ref class="capitalize">{{ label }}</label>
+</template>
+
+<style scoped>
+
+</style>
