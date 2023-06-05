6de43df21ffecfa7e7585e7308f80c7878185757
mykhailo27 <mykhailo.yur27@gmail.com>
2023-06-05 20:26:06 +0200

    replace the table action with search input

--- resources/js/Components/Pages/Table.vue
+++ resources/js/Components/Pages/Table.vue
@@ -8,8 +8,6 @@ import TextInput from "@/Components/TextInput.vue";
 import DangerButton from "@/Components/DangerButton.vue";
 import Pagination from "@/Components/Pagination.vue";
 import {ref} from "vue";
-import Dropdown from "@/Components/Dropdown.vue";
-import DropdownLink from "@/Components/DropdownLink.vue";
 import Select from "@/Components/Select.vue";
 
 const props = defineProps({
@@ -83,43 +81,8 @@ const handleRowClick = (row_id) => {
 
 <template>
     <div class="flex justify-between">
-        <!-- table actions -->
-        <Dropdown align="left" width="48">
-            <template #trigger>
-                <span class="inline-flex rounded-md">
-                    <button
-                        type="button"
-                        class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
-                    >
-                        Actions
-
-                        <svg
-                            class="ml-2 -mr-0.5 h-4 w-4"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                        >
-                            <path
-                                fill-rule="evenodd"
-                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                clip-rule="evenodd"
-                            />
-                        </svg>
-                    </button>
-                </span>
-            </template>
-
-            <template #content>
-                <DropdownLink href="#" :class="'flex justify-between'">
-                    <span>Delete</span>
-                    <span>10</span>
-                </DropdownLink>
-                <DropdownLink href="#" method="post" as="button" :class="'flex justify-between'">
-                    <span>Refresh</span>
-                    <span>10</span>
-                </DropdownLink>
-            </template>
-        </Dropdown>
+        <!-- table search  -->
+        <TextInput type="search" name="search" model-value="" class="focus:border-0" placeholder="Search . . ."/>
 
         <!-- table pagination -->
         <Pagination :links="data.links"/>
