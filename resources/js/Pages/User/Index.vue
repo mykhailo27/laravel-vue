4c96b075ff4fe743cd5e1ee908af2357b7c94dcf
mykhailo27 <mykhailo.yur27@gmail.com>
2023-06-08 22:55:08 +0200

    remove the filter

--- resources/js/Pages/User/Index.vue
+++ resources/js/Pages/User/Index.vue
@@ -4,7 +4,6 @@ import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import {Head} from '@inertiajs/vue3';
 import Table from "@/Components/Pages/Index/Table.vue";
 import Header from "@/Components/Pages/Index/Header.vue";
-import {ref} from "vue";
 
 const props = defineProps({
     users: {
@@ -13,12 +12,6 @@ const props = defineProps({
     },
 });
 
-const filtered_user = ref(props.users);
-
-const getUsers = (data) => {
-    filtered_user.value = data
-}
-
 </script>
 
 <template>
@@ -26,11 +19,11 @@ const getUsers = (data) => {
 
     <AuthenticatedLayout>
         <template #header>
-            <Header @filteredUsers="getUsers" :link="{url: route('users.create'), name: 'Create'}"
+            <Header :link="{url: route('users.create'), name: 'Create'}"
                     :search_url="route('users.index')"/>
         </template>
 
-        <Table :columns="['id', 'name', 'email', 'created_at', 'actions']" :data="filtered_user"
+        <Table :columns="['id', 'name', 'email', 'created_at', 'actions']" :data="users"
                :details_route="route('users.details', {user: '__ROW_ID__'})"/>
 
     </AuthenticatedLayout>
