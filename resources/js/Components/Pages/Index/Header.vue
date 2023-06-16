3523d2cfb04e6e2f781bc605eea4dab616741264
mykhailo27 <mykhailo.yur27@gmail.com>
2023-06-16 21:50:25 +0200

    make the creat btn more visible

--- resources/js/Components/Pages/Index/Header.vue
+++ resources/js/Components/Pages/Index/Header.vue
@@ -42,7 +42,7 @@ const submitSearch = (value) => {
                    @keydown="clearSearchTimer"/>
 
         <Link :href="link.url"
-              class="font-semibold border rounded leading-none w-fit hover:bg-gray-200 hover:shadow-lg active:bg-gray-200 active:shadow-lg">
+              class="bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
             {{ link.name }}
         </Link>
     </div>
