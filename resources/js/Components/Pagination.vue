b10b087ec740c9b5578e322dc1e25110b39ee4af
mykhailo27 <mykhailo.yur27@gmail.com>
2023-06-23 20:13:25 +0200

    update the pagination

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -22,28 +22,35 @@ defineProps({
 
 <template>
     <div class="grid justify-items-center">
-          <span class="text-sm text-gray-700 dark:text-gray-400">
+        <span class="text-sm text-gray-700 dark:text-gray-400">
               Showing
-              <span class="font-semibold text-gray-900 dark:text-white">{{ from }}</span>
+           <span class="font-semibold text-gray-900 dark:text-white">{{ from }}</span>
               to
-              <span class="font-semibold text-gray-900 dark:text-white">{{ to }}</span>
+           <span class="font-semibold text-gray-900 dark:text-white">{{ to }}</span>
               of
-              <span class="font-semibold text-gray-900 dark:text-white">{{ total }}</span>
+           <span class="font-semibold text-gray-900 dark:text-white">{{ total }}</span>
               Entries
-          </span>
-        <nav aria-label="page navigation" v-if="links.length > 3">
-            <ul class="list-style-none flex gap-1">
-                <li v-for="(link, key) in links" :key="key" :aria-current="{'page': link.active}">
+        </span>
+        <nav aria-label="table pagination">
+            <ul class="list-style-none flex items-center">
+                <li v-for="(link, key) in links" :key="key" :aria-current="link.active ? 'page' : null">
                     <a v-if="link.url === null" v-html="link.label"
-                       class="pointer-events-none relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-500 transition-all duration-300 dark:text-neutral-400"
+                       class="pointer-events-none relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-neutral-500 transition-all duration-300 dark:text-neutral-400"
                     />
-
-                    <Link v-else :href="link.url" v-html="link.label"
-                          class="relative block rounded-full bg-transparent text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-                          :class="{'bg-primary-100 font-medium text-primary-700': link.active}"
-                          content-classes="px-3 py-1.5"/>
+                    <Link v-else-if="link.active" :href="link.url" content-classes="px-3 py-1.5"
+                          class="relative block rounded-full bg-primary-100 px-3 py-1.5 text-sm font-medium text-primary-700 transition-all duration-300">
+                        {{ link.label }}
+                        <span
+                            class="absolute -m-px h-px w-px overflow-hidden whitespace-nowrap border-0 p-0 [clip:rect(0,0,0,0)]">
+                            (current)
+                        </span>
+                    </Link>
+                    <Link v-else :href="link.url" v-html="link.label" content-classes="px-3 py-1.5"
+                          class="relative block rounded-full bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100  dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+                    />
                 </li>
             </ul>
         </nav>
     </div>
 </template>
+
