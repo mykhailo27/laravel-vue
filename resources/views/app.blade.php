33df0519083ba8861025b4b3b3caaf7a61d314fa
mykhailo27 <mykhailo.yur27@gmail.com>
2023-06-30 20:13:06 +0200

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
+        <link rel="icon" type="image/png" sizes="16x16" href="https://teamsunday.com/branding/favicon/favicon-16x16.png">
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
