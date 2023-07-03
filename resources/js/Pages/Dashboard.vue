dd14b7c205d5207f0a1e1fe54d928cc33f29e2a7
mykhailo27 <mykhailo.yur27@gmail.com>
2023-07-03 10:28:58 +0200

    add chart to the dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,16 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+import {
+    Chart,
+    initTE,
+} from "tw-elements";
+import {onMounted} from "vue";
+
+onMounted(() => {
+    initTE({ Chart });
+})
+
 </script>
 
 <template>
@@ -11,6 +21,14 @@ import { Head } from '@inertiajs/vue3';
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">Dashboard</h2>
         </template>
 
-        You're logged in!
+        <div class="mx-auto w-3/5 overflow-hidden">
+            <canvas
+                data-te-chart="doughnut"
+                data-te-dataset-label="Traffic"
+                data-te-labels="['Monday', 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday' , 'Saturday' , 'Sunday ']"
+                data-te-dataset-data="[2112, 2343, 2545, 3423, 2365, 1985, 987]"
+                data-te-dataset-background-color="['rgba(63, 81, 181, 0.5)', 'rgba(77, 182, 172, 0.5)', 'rgba(66, 133, 244, 0.5)', 'rgba(156, 39, 176, 0.5)', 'rgba(233, 30, 99, 0.5)', 'rgba(66, 73, 244, 0.4)', 'rgba(66, 133, 244, 0.2)']">
+            </canvas>
+        </div>
     </AuthenticatedLayout>
 </template>
