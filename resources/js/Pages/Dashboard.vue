3c052b148ce468dd406448ba267929367b435d99
mykhailo27 <mykhailo.yur27@gmail.com>
2023-07-03 13:23:15 +0200

    update the dashboard data

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -25,10 +25,11 @@ onMounted(() => {
             <canvas
                 data-te-chart="doughnut"
                 data-te-dataset-label="Traffic"
-                data-te-labels="['Monday', 'Tuesday' , 'Wednesday' , 'Thursday' , 'Friday' , 'Saturday' , 'Sunday ']"
-                data-te-dataset-data="[2112, 2343, 2545, 3423, 2365, 1985, 987]"
+                data-te-labels="['Scheduled', 'Sent' , 'Delivered' , 'Returned' , 'Tree planted']"
+                data-te-dataset-data="[2112, 2343, 2545, 3423, 2365]"
                 data-te-dataset-background-color="['rgba(63, 81, 181, 0.5)', 'rgba(77, 182, 172, 0.5)', 'rgba(66, 133, 244, 0.5)', 'rgba(156, 39, 176, 0.5)', 'rgba(233, 30, 99, 0.5)', 'rgba(66, 73, 244, 0.4)', 'rgba(66, 133, 244, 0.2)']">
             </canvas>
         </div>
+
     </AuthenticatedLayout>
 </template>
